df771e439ab1c57fe47e305cfda5e5f2e910ce3e
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-03-24 22:01:31 +0100

    add:img

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ import express from 'express';
 
 //impostiamo express e la porta del server
 const app = express();
+import cors from 'cors';
 const port = 3000;
 
 
 import movieRouter from './routes/movieRouter.js';
+import imagePathMiddleware from './middlewares/imagePath.js';
 
+
+//middleware cors 
+app.use(
+    cors({
+      origin: process.env.FRONTEND_APP,
+    })
+);
+
+
+//middleware per gestire asset statici
+app.use(express.static('public'));
+
+//middleware per gestire le informazioni del body
+app.use(express.json());
+
+//middleware per gestione delle immagini
+app.use(imagePathMiddleware);
 
 //Router 
 app.use( '/movies' , movieRouter ) 
